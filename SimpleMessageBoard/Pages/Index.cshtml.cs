65b7066b1fe1870e63c93426ae3683913259f1a6
Adam <adammarkperkins@gmail.com>
2021-08-08 01:07:03 +0100

    removed extra using

--- SimpleMessageBoard/Pages/Index.cshtml.cs
+++ SimpleMessageBoard/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace SimpleMessageBoard.Pages
